ff96d23d28503f12f743a3cdebf0c79ff890dc02
Amit Kumar <amit.kumar35b14f@cognizant.com>
2019-12-30 23:43:10 +0530

    done

--- src/main/java/com/stackroute/keepnote/aspect/LoggingAspect.java
+++ src/main/java/com/stackroute/keepnote/aspect/LoggingAspect.java
@@ -1,12 +1,56 @@
 package com.stackroute.keepnote.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /* Annotate this class with @Aspect and @Component */
-
+@Component
+@Aspect
 public class LoggingAspect {
-
 	/*
 	 * Write loggers for each of the methods of controller, any particular method
 	 * will have all the four aspectJ annotation
 	 * (@Before, @After, @AfterReturning, @AfterThrowing).
 	 */
+	@Pointcut(value = "execution(* com.stackroute.keepnote.controller.*.*(..))")
+	public void beforeController() {
+	}
+
+	@Before("beforeController()")
+	public void beforeAspect(JoinPoint joinpoint) {
+		System.out.println("-------@Before(\"beforeController()\"----------"+joinpoint.getSignature());
+
+	}
+
+	@Pointcut(value = "execution(* com.stackroute.keepnote.controller.*.*(..))")
+	public void afterController() {
+	}
+
+	@After("afterController()")
+	public void afterAspect(JoinPoint joinpoint) {
+		System.out.println("----------@After(afterController()"+joinpoint.getSignature());
+	}
+
+	@Pointcut(value = "execution(* com.stackroute.keepnote.controller.*.*(..))")
+	public void afterThrowing() {
+	}
+
+	@AfterThrowing(pointcut="execution(* com.stackroute.keepnote.controller.*.*(..))",throwing="exception")
+	public void afterThrowingAspect(Exception exception) {
+		System.out.println("----@AfterThrowing---"+exception.getMessage());
+	}
+	@Pointcut(value = "execution(* com.stackroute.keepnote.controller.*.*(..))")
+	public void afterReturning() {
+	}
+
+	@AfterReturning(pointcut="execution(* com.stackroute.keepnote.controller.*.*(..))",returning="val")
+	public void afterReturningAspect(Object val) {
+		System.out.println("----@AfterReturning---"+val);
+	}
 }
--- src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/keepnote/config/ApplicationContextConfig.java
@@ -1,5 +1,26 @@
 package com.stackroute.keepnote.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.stackroute.keepnote.model.Category;
+import com.stackroute.keepnote.model.Note;
+import com.stackroute.keepnote.model.Reminder;
+import com.stackroute.keepnote.model.User;
+
 /*This class will contain the application-context for the application. 
  * Define the following annotations:
  * @Configuration - Annotating a class with the @Configuration indicates that the 
@@ -12,7 +33,11 @@ package com.stackroute.keepnote.config;
  *                  
  * @EnableAspectJAutoProxy - This spring aop annotation is used to enable @AspectJ support with Java @Configuration  
  * */
-
+@Configuration
+@ComponentScan("com.stackroute.keepnote")
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class ApplicationContextConfig {
 
 	/*
@@ -30,7 +55,30 @@ public class ApplicationContextConfig {
 	 * dataSource.setUsername(System.getenv("MYSQL_USER"));
 	 * dataSource.setPassword(System.getenv("MYSQL_PASSWORD"));
 	 */
+	/*
+	 * @Bean
+	 * 
+	 * public DataSource dataSource() { DriverManagerDataSource dataSource = new
+	 * DriverManagerDataSource();
+	 * dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+	 * dataSource.setUrl("jdbc:mysql://" + System.getenv("MYSQL_HOST") + ":3306/" +
+	 * System.getenv("MYSQL_DATABASE") +
+	 * "?verifyServerCertificate=false&useSSL=false&requireSSL=false");
+	 * dataSource.setUsername(System.getenv("MYSQL_USER"));
+	 * dataSource.setPassword(System.getenv("MYSQL_PASSWORD")); return dataSource; }
+	 */
 
+	
+	  @Bean public DataSource dataSource() { DriverManagerDataSource dataSource =
+	  new DriverManagerDataSource();
+	  dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); dataSource
+	  .setUrl(
+	  "jdbc:mysql://localhost:3306/test?verifyServerCertificate=false&useSSL=false&requireSSL=false"
+	  ); dataSource.setUsername("root"); dataSource.setPassword("GayatriMata1@.");
+	  return dataSource;
+	  
+	  }
+	 
 	/*
 	 * create a getter for Hibernate properties here we have to mention 1. show_sql
 	 * 2. Dialect 3. hbm2ddl
@@ -40,6 +88,19 @@ public class ApplicationContextConfig {
 	 * Define the bean for SessionFactory. Hibernate SessionFactory is the factory
 	 * class through which we get sessions and perform database operations.
 	 */
+	@Bean
+	@Autowired
+	public LocalSessionFactoryBean sessionFactory(DataSource datasource) {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(datasource);
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		properties.put("hibernate.show_sql", "true");
+		properties.put("hibernate.hbm2ddl.auto", "update");
+		sessionFactory.setHibernateProperties(properties);
+		sessionFactory.setAnnotatedClasses(Category.class, Note.class, Reminder.class, User.class);
+		return sessionFactory;
+	}
 
 	/*
 	 * Define the bean for Transaction Manager. HibernateTransactionManager handles
@@ -49,5 +110,11 @@ public class ApplicationContextConfig {
 	 * JDBC too. HibernateTransactionManager allows bulk update and bulk insert and
 	 * ensures data integrity.
 	 */
-
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager txManager = new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory);
+		return txManager;
+	}
 }
